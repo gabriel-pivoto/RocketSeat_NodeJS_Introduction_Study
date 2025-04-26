9b3f8cd66d7a2756501992a2ae9267f3cd9c9c75
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-04-26 09:41:29 -0300

    feat: Create Readable stream

---
+++ streams/fundamentals.js
@@ -0,0 +1,22 @@
+//streams
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+class OneToHundredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+        return;
+      }
+      const buf = Buffer.from(String(i));
+      this.push(buf);
+    }, 1000);
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout);
